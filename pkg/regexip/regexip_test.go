3b4a8cc94ff48fb4cb2bb2e9582d7ad6cea4b32a
Marco Ferragina <marco.ferragina@gmail.com>
2023-08-04 19:10:21 +0200

    regexip tests

---
+++ pkg/regexip/regexip_test.go
@@ -0,0 +1,63 @@
+package regexip
+
+import (
+	"log"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/miekg/dns"
+)
+
+const testListenAddress = "127.0.0.1:6353"
+
+func startServer() *dns.Server {
+	n := dns.HandlerFunc(func(w dns.ResponseWriter, m *dns.Msg) {})
+	rxip := &Handler{
+		Next: n,
+	}
+	dns.Handle("pig.io.", rxip)
+
+	server := &dns.Server{
+		Addr: testListenAddress,
+		Net:  "udp",
+	}
+	go func() {
+		log.Printf("listening on %s", testListenAddress)
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatalf("failed to start server: %s\n ", err.Error())
+		}
+	}()
+
+	return server
+}
+
+func TestA(t *testing.T) {
+	server := startServer()
+	defer server.Shutdown()
+
+	time.Sleep(1 * time.Second)
+
+	testsA := map[string]string{
+		"127-0-0-1.pig.io":                "127.0.0.1",
+		"prefix-127-0-0-1.pig.io":         "127.0.0.1",
+		"127-0-0-1-suffix.pig.io":         "127.0.0.1",
+		"prefix-127-0-0-1-suffix.pig.io":  "127.0.0.1",
+		"a-b-c-192-168-10-1-a-b-c.pig.io": "192.168.10.1",
+	}
+	for k, v := range testsA {
+		m := new(dns.Msg)
+		m.SetQuestion(k+".", dns.TypeA)
+		resp, err := dns.Exchange(m, testListenAddress)
+		if err != nil {
+			t.Fatalf("Expected to receive reply, but didn't: %s", err)
+		}
+		rr := resp.Answer[0]
+		a := rr.(*dns.A)
+		ip := net.ParseIP(v)
+		if !a.A.Equal(ip) {
+			t.Fatalf("expected ip '%s' got '%s'", v, a.A)
+		}
+	}
+}
