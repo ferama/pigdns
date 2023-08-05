0d92befa92ffa1d3371e422436a06068cbf33de5
Marco Ferragina <marco.ferragina@gmail.com>
2023-08-05 12:43:09 +0200

    ipv6 test for regexip

--- pkg/regexip/regexip_test.go
+++ pkg/regexip/regexip_test.go
@@ -53,11 +53,45 @@ func TestA(t *testing.T) {
 		if err != nil {
 			t.Fatalf("Expected to receive reply, but didn't: %s", err)
 		}
+		if len(resp.Answer) != 1 {
+			t.Fatal("expeected one answer")
+		}
 		rr := resp.Answer[0]
 		a := rr.(*dns.A)
 		ip := net.ParseIP(v)
 		if !a.A.Equal(ip) {
 			t.Fatalf("expected ip '%s' got '%s'", v, a.A)
+		}
+	}
+}
+
+func TestAAAA(t *testing.T) {
+	server := startServer()
+	defer server.Shutdown()
+
+	time.Sleep(1 * time.Second)
+
+	testsAAAA := map[string]string{
+		"2001-0db8--1428-57ab.pig.io":               "2001:0db8::1428:57ab",
+		"prefix-2001-0db8--1428-57ab.pig.io":        "2001:0db8::1428:57ab",
+		"2001-0db8--1428-57ab-suffix.pig.io":        "2001:0db8::1428:57ab",
+		"prefix-2001-0db8--1428-57ab-suffix.pig.io": "2001:0db8::1428:57ab",
+	}
+	for k, v := range testsAAAA {
+		m := new(dns.Msg)
+		m.SetQuestion(k+".", dns.TypeAAAA)
+		resp, err := dns.Exchange(m, testListenAddress)
+		if err != nil {
+			t.Fatalf("Expected to receive reply, but didn't: %s", err)
+		}
+		if len(resp.Answer) != 1 {
+			t.Fatal("expeected one answer")
+		}
+		rr := resp.Answer[0]
+		aaaa := rr.(*dns.AAAA)
+		ip := net.ParseIP(v)
+		if !aaaa.AAAA.Equal(ip) {
+			t.Fatalf("expected ip '%s' got '%s'", v, aaaa.AAAA)
 		}
 	}
 }
