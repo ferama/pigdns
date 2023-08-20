5db87f38fcae1c63fab28d6988380d0a0194db68
Marco Ferragina <marco.ferragina@gmail.com>
2023-08-20 20:00:30 +0200

    fix issue with doh

--- pkg/server/chains.go
+++ pkg/server/chains.go
@@ -10,6 +10,7 @@ import (
 	"github.com/ferama/pigdns/pkg/handlers/root"
 	"github.com/ferama/pigdns/pkg/handlers/zone"
 	"github.com/ferama/pigdns/pkg/pigdns"
+	"github.com/miekg/dns"
 )
 
 // BuildResolverHandler creates an handler that resolves recursively
@@ -18,11 +19,19 @@ func BuildResolverHandler(datadir string, allowedNets []string) pigdns.Handler {
 	var chain pigdns.Handler
 
 	chain = pigdns.HandlerFunc(func(ctx context.Context, r *pigdns.Request) {
-		r.ResponseWriter.Close()
+		// I cannot close the connection here but I need to give an answer
+		// anyway. Closing the connections prevents DOH to work properly from
+		// browser
+		// r.ResponseWriter.Close()
+
+		m := new(dns.Msg)
+		m.Authoritative = false
+		m.Rcode = dns.RcodeServerFailure
 		if ctx.Value(collector.CollectorContextKey) != nil {
 			cc := ctx.Value(collector.CollectorContextKey).(*collector.CollectorContext)
 			cc.AnweredBy = "failure"
 		}
+		r.Reply(m)
 	})
 	chain = resolver.NewResolver(chain, datadir, allowedNets)
 	chain = &collector.Handler{Next: chain}
